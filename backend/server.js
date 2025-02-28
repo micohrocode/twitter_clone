78b2659ac0834b779806801f9fdfbf501e1ebf58
micohrocode <michaelrocode@gmail.com>
2025-02-28 13:08:56 -0500

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server is running...")
+import express from "express"
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Server is ready")
+})
+
+app.listen(8000, ()=>{
+    console.log("Server is running on port 8000")
+})
