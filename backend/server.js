6a95d68e4dd4d25a8e81349a902c90a31d5f59ed
micohrocode <michaelrocode@gmail.com>
2025-02-28 13:18:42 -0500

    inital routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express"
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express()
 
-app.get("/",(req,res)=>{
-    res.send("Server is ready")
-})
+app.use("/api/auth",authRoutes)
 
 app.listen(8000, ()=>{
     console.log("Server is running on port 8000")
