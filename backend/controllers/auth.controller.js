345ab006a11fc7d31fd07c77f68f0afd7ab23a86
micohrocode <michaelrocode@gmail.com>
2025-02-28 15:02:04 -0500

    logout logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,5 +92,11 @@ export const login = async (req,res)=>{
 }
 
 export const logout = async (req,res)=>{
-    
+    try {
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message: "Logged out successfully"})
+    } catch (error) {
+        console.error("Error in logout controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
 }
