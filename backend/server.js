79bcad89cefd010d0047ddfa5d76b6b4949e7407
micohrocode <michaelrocode@gmail.com>
2025-03-03 16:19:56 -0500

    user update profile added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import {v2 as cloudinary} from "cloudinary"
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -7,6 +8,12 @@ import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser"
 
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
+})
 
 const app = express()
 const PORT = process.env.PORT || 5000;
