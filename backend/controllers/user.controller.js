5df4057b71b4bd6ddf68c55c352a561175e127c2
micohrocode <michaelrocode@gmail.com>
2025-03-03 15:27:56 -0500

    added notifications

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async(req, res) => {
     const {username} = req.params;
@@ -39,6 +40,16 @@ export const followUnfollowUser = async (req, res) => {
             // Follow a user
             await User.findByIdAndUpdate(id, { $push: {followers: req.user._id }})
             await User.findByIdAndUpdate(req.user._id, { $push: {following: id} })
+
+
+            const newNotification = new Notification({
+                type:"follow",
+                from: req.user._id,
+                to: userToModify._id
+            })
+
+            await newNotification.save()
+
             res.status(200).json({message : "User followed successfully"})
         }
 
